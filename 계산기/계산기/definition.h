96988f66811c72ae6b9fc1c678bc2e0cabe71769
PragmoB <peullaegeumo@gmail.com>
2022-07-06 21:57:44 +0900

    main menu update

---
+++ 계산기/계산기/definition.h
@@ -0,0 +1,9 @@
+#ifndef DEF_H
+#define DEF_H
+
+int add(int operand1, int operand2);
+int sub(int operand1, int operand2);
+int mul(int operand1, int operand2);
+int divi(int operand1, int operand2);
+
+#endif // DEF_H
